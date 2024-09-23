3b7efb994be6ffdcac59a2dc37e124cb45ba146f
shikaan <spagnolo.manu@gmail.com>
2024-09-23 23:40:26 +0200

    feat: use calling conventions already

--- 03-nullary-functions.asm
+++ 03-nullary-functions.asm
@@ -23,6 +23,8 @@ _start:
   ; in the range [1, faces] and store it in `rax`.
   call random
 
+  ; We store the result in `rdi` to print it later.
+  mov rdi, rax
   ; We can print our single-digit number on screen.
   call print_digit
 
@@ -54,34 +56,34 @@ random:
   ; We store the result in `rax` to print it later.
   mov rax, rdx
   ; We increment the result to adjust the range to
-  ; [1, faces]. In high-levelese, this would be `rax++`.
+  ; [1, faces]. In high-leveallese, this would be `rax++`.
   inc rax
 
   ; We are done, let's return to the caller.
   ret
 
 print_digit:
-  ; This function prints a single digit number in `rax`
+  ; This function prints a single digit number in `rdi`
   ; to the screen, using the buffer we defined earlier. 
 
   ; First, we convert the number to its character 
   ; representation. As you can see in any ASCII table,
   ; the digits are in the range [48, 57].
   ; Adding 48 yields the ASCII value of the character.
-  add rax, 48
+  add rdi, 48
 
   ; We want to replace the first character in the
   ; buffer with the digit.
   ;
   ; The [] syntax is used to get the memory location
   ; of what's inside the brackets. In plain English, 
-  ; `[thing]` means "address where `thing` is stored".
-  ; We use `al` to access the lower byte of `rax` as
-  ; we saw in the first lesson.
+  ; `[thing]` means "memory location whose address is
+  ; thing".  We use `dil` to access the lower byte of 
+  ; `rdi` as we saw in the first lesson.
   ;
   ; So, this instruction means "store the lower byte
-  ; of `rax` in the memory location of `buffer`".
-  mov [buffer], al
+  ; of `rdi` in the memory location of `buffer`".
+  mov [buffer], dil
   
   ; This is the print routine we've seen before.
   mov rax, sys_write
