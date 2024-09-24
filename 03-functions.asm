a2abff748547a919aeaa9cc6432f75cf2c1dd40c
shikaan <spagnolo.manu@gmail.com>
2024-09-24 23:48:15 +0200

    feat: dungeon crawler nullary function example

--- 03-functions.asm
+++ 03-functions.asm
@@ -1,42 +1,101 @@
 section .data
-  SYS_EXIT  equ 60
-  SYS_WRITE equ 1
-  buffer    db  0, 0
+  ; This is our usual set of constants for syscalls.
+  sys_write equ 1
+  sys_exit  equ 60
+  fd_stdout equ 1
+
+  ; These are the input parameters of our program.
+  ; We will roll a die with `faces` faces.
+  faces     equ 6
+
+  ; The result will be print to the screen. We need a
+  ; buffer to store the result, so we can print it.
+  buffer    db `0\n`
+  len       equ 2
 
 section .text
-global _start
+  global _start
 _start:
-  mov rdi, 1
-  call to_string
+  ; Our program is nicely structured in functions.
+  ; Go check them out when you encounter a `call`!
+
+  ; First thing we do is generating a random number
+  ; in the range [1, faces] and store it in `rax`.
+  call random
+
+  ; We store the result in `rdi` to print it later.
   mov rdi, rax
-  mov rsi, 2
-  call print
-  mov rdi, 0
+  ; We can print our single-digit number on screen.
+  call print_digit
+
+  ; We can now exit.
   call exit
 
-; exits with status code in rdi
+random:
+  ; This function generates a random number in the
+  ; range [1, faces] using the hardware RNG and 
+  ; scaling the result with a modulo operation.
+  
+  ; rdrand generates a random number in the range
+  ; [0, 2^64-1] and stores it in `rax`.
+  rdrand rax
+
+  ; To get the modulo of a number, we perform a
+  ; division with `div` and collect the remainder.
+  ; `div` divides the value in `rax` by the operand,
+  ; saves the result back in `rax` and the remainder
+  ; in `rdx`.
+  ;
+  ; Here we divide the random number by the number of
+  ; faces of the die. The remainder will be the result
+  ; of the roll.
+  mov rsi, faces
+  mov rdx, 0
+  div rsi
+
+  ; We store the result in `rax` to print it later.
+  mov rax, rdx
+  ; We increment the result to adjust the range to
+  ; [1, faces]. In high-leveallese, this would be `rax++`.
+  inc rax
+
+  ; We are done, let's return to the caller.
+  ret
+
+print_digit:
+  ; This function prints a single digit number in `rdi`
+  ; to the screen, using the buffer we defined earlier. 
+
+  ; First, we convert the number to its character 
+  ; representation. As you can see in any ASCII table,
+  ; the digits are in the range [48, 57].
+  ; Adding 48 yields the ASCII value of the character.
+  add rdi, 48
+
+  ; We want to replace the first character in the
+  ; buffer with the digit.
+  ;
+  ; The [] syntax is used to get the memory location
+  ; of what's inside the brackets. In plain English, 
+  ; `[thing]` means "memory location whose address is
+  ; thing".  We use `dil` to access the lower byte of 
+  ; `rdi` as we saw in the first lesson.
+  ;
+  ; So, this instruction means "store the lower byte
+  ; of `rdi` in the memory location of `buffer`".
+  mov [buffer], dil
+  
+  ; This is the print routine we've seen before.
+  mov rax, sys_write
+  mov rsi, buffer
+  mov rdx, len
+  mov rdi, fd_stdout
+  syscall
+
+  ; Once again, we can return to the caller.
+  ret
+
 exit:
-  mov rax, SYS_EXIT
+  mov rax, sys_exit
+  mov rdi, 0
   syscall
-  ret
-
-; prints a buffer [rdi] with given lenght [rsi]
-print:
-  ; perform syscall
-  mov rax, SYS_WRITE
-  mov rdx, rsi
-  mov rsi, rdi
-  mov rdi, 1
-  syscall
-  ret
-
-; converts single-digit int to string
-to_string:
-  mov rax, rdi
-  add rax, 48
-  mov [buffer], rax
-  mov byte [buffer+1], 0
-  mov rax, buffer
-  ret
-
-
--- 03-nullary-functions.asm
+++ 03-nullary-functions.asm
@@ -1,101 +1,150 @@
 section .data
-  ; This is our usual set of constants for syscalls.
-  sys_write equ 1
-  sys_exit  equ 60
-  fd_stdout equ 1
+  ; Here we define our usual constants. No surprises.
+  sys_write  equ 1
+  sys_exit   equ 60
+  fd_stdout  equ 1
+  
+  ; This is the number of doors in our dungeon.
+  doors equ 5
 
-  ; These are the input parameters of our program.
-  ; We will roll a die with `faces` faces.
-  faces     equ 6
+  ; These are the messages our explorer will see in 
+  ; the game. Instead of counting characters, we use
+  ; `$-` to get the string length.
+  ; We will use the `$` operator only to calculate
+  ; string length in this course. Feel free to dig
+  ; deeper later, if you want.
+  door        db `You open a door...\n`
+  door_l      equ $-door
 
-  ; The result will be print to the screen. We need a
-  ; buffer to store the result, so we can print it.
-  buffer    db `0\n`
-  len       equ 2
+  empty       db `> This room is empty. Keep going.\n`
+  empty_l     equ $-empty
+
+  treasure    db `> You found the treasure. Joy!\n`
+  treasure_l  equ $-treasure
+
+  fail        db `\n  No treasure in the dungeon. What a scam!\n`
+  fail_l      equ $-fail
 
 section .text
-  global _start
+global _start
 _start:
-  ; Our program is nicely structured in functions.
-  ; Go check them out when you encounter a `call`!
+  ; In this game we will open a few doors in search of
+  ; a treasure. If we find it, we exit the program.
+  ;
+  ; The rbx register will be used to count the number
+  ; of doors we opened. We zero it for good measure.
+  mov rbx, 0
 
-  ; First thing we do is generating a random number
-  ; in the range [1, faces] and store it in `rax`.
-  call random
+  ; We're opeing the first door... exciting!
+  call open_door
 
-  ; We store the result in `rdi` to print it later.
-  mov rdi, rax
-  ; We can print our single-digit number on screen.
-  call print_digit
+open_door:
+  ; Let's tell our brave explorer what's happening.
+  call print_open_door
 
-  ; We can now exit.
-  call exit
-
-random:
-  ; This function generates a random number in the
-  ; range [1, faces] using the hardware RNG and 
-  ; scaling the result with a modulo operation.
-  
+  ; To check if there is a treasure in the room, we
+  ; generate a random number. If it's odd, we have 
+  ; found the treasure, else we keep seraching.
+  ;
   ; rdrand generates a random number in the range
   ; [0, 2^64-1] and stores it in `rax`.
   rdrand rax
+  ; An efficient way of checking if a number is odd
+  ; is to look at the last digit of its binary
+  ; representation: if it's 1, it's odd.
+  ; We do that using a bitwise and against 1.
+  ; In many high-level languages you'd see `rax & 1` 
+  and rax, 1
 
-  ; To get the modulo of a number, we perform a
-  ; division with `div` and collect the remainder.
-  ; `div` divides the value in `rax` by the operand,
-  ; saves the result back in `rax` and the remainder
-  ; in `rdx`.
+  ; Just like in the previous lesson, this is an if.
+  ; Is the number odd?
+  cmp rax, 1
+  je handle_trasure
+  call print_empty
+
+  ; rbx is the number of checked doors.
+  ; 
+  ; We opened a door, so we `inc` (increase) rbx.
+  ; In high-levelese it would be like `rbx++`
+  inc rbx 
+  
+  ; If `rbx` equals `doors`, we have checked all the
+  ; doors and there is no treasure. How sad!
+  cmp rbx, doors
+  je handle_fail
+  
+  ; Else, we can keep searching by jumping back at the 
+  ; beginning of this block. Totally uneventful. 
+  ; Except that we JUST IMPLEMENTED A LOOP! 
+  ; 
+  ; In fact, this equates to
+  ;   for (rbx = 0; rbx < doors; rbx++)
   ;
-  ; Here we divide the random number by the number of
-  ; faces of the die. The remainder will be the result
-  ; of the roll.
-  mov rsi, faces
-  mov rdx, 0
-  div rsi
+  ; - rbx was zeroed in the beginning
+  ; - we increment rbx on every iteration
+  ; - as soon as rbx == doors we break
+  ; 
+  ; Tada!
+  jmp open_door
 
-  ; We store the result in `rax` to print it later.
-  mov rax, rdx
-  ; We increment the result to adjust the range to
-  ; [1, faces]. In high-leveallese, this would be `rax++`.
-  inc rax
-
-  ; We are done, let's return to the caller.
+print_open_door:  
+  ; All the print_* work the same. We explain them here.
+  ; 
+  ; To reduce boilerplate, we put the shared code
+  ; for printing in the `print` routine. Check it out!
+  ;
+  ; Done? Here we just complete the picture, putting
+  ; the string and its length in the right registers
+  ; before calling `print`.
+  mov rsi, door
+  mov rdx, door_l
+  call print
   ret
 
-print_digit:
-  ; This function prints a single digit number in `rdi`
-  ; to the screen, using the buffer we defined earlier. 
+print_empty: 
+  ; See `print_open_door`.
+  mov rsi, empty
+  mov rdx, empty_l
+  call print
+  ret
 
-  ; First, we convert the number to its character 
-  ; representation. As you can see in any ASCII table,
-  ; the digits are in the range [48, 57].
-  ; Adding 48 yields the ASCII value of the character.
-  add rdi, 48
+handle_trasure:
+  ; We found the treasure! 
+  ; Let's congratulate our explorer and then exit.
+  call print_treasure
+  call exit
 
-  ; We want to replace the first character in the
-  ; buffer with the digit.
-  ;
-  ; The [] syntax is used to get the memory location
-  ; of what's inside the brackets. In plain English, 
-  ; `[thing]` means "memory location whose address is
-  ; thing".  We use `dil` to access the lower byte of 
-  ; `rdi` as we saw in the first lesson.
-  ;
-  ; So, this instruction means "store the lower byte
-  ; of `rdi` in the memory location of `buffer`".
-  mov [buffer], dil
-  
-  ; This is the print routine we've seen before.
+print_treasure:
+  ; See `print_open_door`.
+  mov rsi, treasure
+  mov rdx, treasure_l
+  call print
+  ret
+
+handle_fail:
+  ; No treasures. Bummer.
+  ; Let's tell our player and then exit.
+  call print_fail
+  call exit
+
+print_fail:
+  ; See `print_open_door`.
+  mov rsi, fail
+  mov rdx, fail_l
+  call print
+  ret
+
+print:
+  ; The same routine as in the last few articles, but
+  ; it expects the caller to have set rsi (string) and
+  ; rdx (string length) beforehand
   mov rax, sys_write
-  mov rsi, buffer
-  mov rdx, len
   mov rdi, fd_stdout
   syscall
-
-  ; Once again, we can return to the caller.
   ret
 
 exit:
+  ; Same old exit routine we have seen many times.
   mov rax, sys_exit
   mov rdi, 0
   syscall
